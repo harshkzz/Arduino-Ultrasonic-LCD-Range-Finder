3abb80898287e8acd4a71486447c5b4e03cea4e5
Harsh Kumar <125388784+harshkzz@users.noreply.github.com>
2023-03-02 00:02:37 +0530

    Add files via upload

---
+++ Lcd_range.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F, 16, 2);         // CHANGE ADDRESS HERE ACCORDINGLY
+#define echoPin 7                           // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
+#define trigPin 8                           // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
+long duration, distance;
+void setup(){
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  lcd.init();
+  lcd.backlight();
+  lcd.blink_on();
+}
+void loop(){
+  delay(150);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration / 58.2;
+  String disp = String(distance);
+  lcd.clear();
+  lcd.print("distance :");
+  lcd.setCursor(0, 1);
+  lcd.print(disp);
+  lcd.print(" cm");
+}
